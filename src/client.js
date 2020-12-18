7257904b9553a4c4c2499355f4cd6d8cd0dc3878
Xiexuc <liachenxiexu@gmail.com>
2020-12-18 15:24:34 +1100

    update frontend static interface

--- src/client.js
+++ src/client.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import useFetch from 'react-fetch-hook';
+import './client.css'
 
 
 function App() {
-    const {isLoading, data} = useFetch('http://localhost:4001');
+    const [ instructions, setInstructions ] = useState('');
+    const { isLoading, data } = useFetch('http://localhost:4001');
     if(isLoading) {
         return 'Loading...';
     }
-    return <pre>{JSON.stringify(data, null, 4)}</pre>;
+
+    const handleChange = (event) => {
+        setInstructions(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventdefault();
+        
+    }
+
+    return (
+        <div className="app">
+            <h1 className="title">Drone Demo</h1>
+            <section className="content">
+                <div className="container">
+                    <textarea value={instructions} onChange={handleChange} className="input" type="text" placeholder="Enter your instruction here..."></textarea>
+                </div>
+                <button onClick={handleSubmit} className="btn" type="submit">Send</button>
+                <div className="container">
+                    <div>{JSON.stringify(data)}</div>
+                </div>
+            </section>
+        </div>
+    );
 }
 
 
