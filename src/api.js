ca26efed3944ad2b9658c638e6b34b2478e730f2
Xiexuc <liachenxiexu@gmail.com>
2020-12-18 18:07:16 +1100

    update backend

--- src/api.js
+++ src/api.js
@@ -5,9 +5,16 @@ const path = require('path');
 
 
 app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.json({foo: 'bar'});
+    res.json({ foo: 'bar' });
+});
+
+app.post('/', (req, res) => {
+    const { instructions } = req.body;
+
+    res.json({ number: instructions });
 });
 
 app.listen(4001, () => console.log(`Api started at http://localhost:4001`));
