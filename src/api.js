cd1aff65253ffe2c62fd4303e39b50523794e000
Xiexuc <liachenxiexu@gmail.com>
2020-12-19 13:25:38 +1100

    update Part 1

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require('path');
-const { countChar, removeDuplicateX, removeCharAfterLastX } = require ('./utils/function');
+const { countChar, removeDuplicateX, countBillboards } = require ('./utils/function');
 
 
 app.use(cors());
@@ -20,13 +20,10 @@ app.post('/', (req, res) => {
     let string = removeDuplicateX(instructions);
     console.log(string);
 
-    //2. Remove the characters after the last X
-    string = removeCharAfterLastX(string);
+    //2. Count photos
+    const result = countBillboards(string);
 
-
-    //3. Remove consecutive opposite direction characters
-
-    res.json({ number: string });
+    res.json({ number: result });
 });
 
 app.listen(4001, () => console.log(`Api started at http://localhost:4001`));
--- src/utils/function.js
+++ src/utils/function.js
@@ -1,5 +1,5 @@
 const countChar = (string, char) => {
-    const stringArr = [...string]; 
+    const stringArr = [...string];
     let num = 0;
     stringArr.forEach(i => {
         i === char && num++;
@@ -44,18 +44,51 @@ const removeDuplicateX = (string) => {
     return result.join('');
 }
 
-const removeCharAfterLastX = (string) => {
-    const stringArr = [...string];
-    const lastIndex = stringArr.lastIndexOf("x");
-    const result = stringArr.slice(0, lastIndex + 1);
-    return result.join('');
-}
+const countBillboards = string => {
+    const moveArr = string.split("x");
+    const box = [];
+    let currentPosition = [0, 0];
+    let nextPosition;
+    // Here i<moveArr.length-1 is used because the last element in moveArr can only be '' or '{direaction}', both of them has no effect on the final calculation result, so there is no need to bring it into the loop
+    for (let i = 0; i < moveArr.length - 1; i++) {
+        const nextMove = moveArr[i];
+        // if start with 'x'.
+        if (nextMove === '') {
+            box.push([...currentPosition]);
+        } else {
+            nextPosition = handleMove(currentPosition, nextMove);
+            //if start with direction and box is empty.
+            if (box.length === 0) {
+                box.push([...nextPosition]);
+            } else {
+                //if box is not empty，then validate whether the box has the photo
+                const isPositionExist = arrayHasElement(box, nextPosition);
+                !isPositionExist && box.push([...nextPosition]);
+                console.log(box, 'box3')
+            }
+            // Relocation
+            currentPosition = nextPosition;
+        }
+    }
 
-const getMoveBetweenX = (string) => {
-    const direactionArr = string.split('x');
-    const getEffectDireaction = direactionArr.map(direaction => {
+    return box.length;
+};
 
-    })
+// Check whether an array element exist in a 2D array
+const arrayHasElement = function (array, element) {
+    for (let el of array) {
+        if (el.length === element.length) {
+            for (let index in el) {
+                if (el[index] !== element[index]) {
+                    break;
+                }
+                if (index == (el.length - 1)) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
 }
 
 
@@ -63,5 +96,5 @@ const getMoveBetweenX = (string) => {
 module.exports = {
     countChar,
     removeDuplicateX,
-    removeCharAfterLastX
+    countBillboards
 };
